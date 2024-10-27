dbe172081bfe1ab4bb7565089b575c810d9a5a79
isoulflybm <isoulfly.bm@gmail.com>
2024-10-27 22:31:41 +0200

    Peerjs fix

--- web/js/peerjs-messenger.js
+++ web/js/peerjs-messenger.js
@@ -2,7 +2,6 @@ window.addEventListener('load', () => {
 	peer = new Peer();
 	peer.on('open', (id) => {
 		console.log('Open ID ' + id);
-        peer.connect(id);
 	});
     peer.on('connect', (connection) => {
     });
